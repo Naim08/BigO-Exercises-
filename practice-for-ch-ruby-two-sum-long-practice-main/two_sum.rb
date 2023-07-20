d39466555425fa65431d54b58e560cfc415344bd
Naim Miah <mmiah@fordham.edu>
2023-07-20 17:25:29 -0400

    hash method

--- practice-for-ch-ruby-two-sum-long-practice-main/two_sum.rb
+++ practice-for-ch-ruby-two-sum-long-practice-main/two_sum.rb
@@ -1,18 +1,31 @@
-def two_sum?(arr, target)
+# def two_sum?(arr, target)
 
-    i = 0 
-    while i < arr.length - 1
-        j = i + 1 
-        while j < arr.length
-            if arr[i] + arr[j] == target
+#     i = 0
+#     while i < arr.length - 1
+#         j = i + 1
+#         while j < arr.length
+#             if arr[i] + arr[j] == target
+#                 return true
+#             end
+#             j += 1
+#         end
+#         i += 1
+#     end
+#     false
+# end
+
+
+def two_sum?(arr, target)
+      hash = {}
+        arr.each do |ele|
+            if hash[target - ele]
                 return true
             end
-            j += 1
+            hash[ele] = true
         end
-        i += 1
-    end
-    false
+        false
 end
+
 
 arr = [0, 1, 2, 5, 7]
 p two_sum?(arr, 6) # => should be true
