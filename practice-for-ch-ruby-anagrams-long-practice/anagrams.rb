d93f5a12e8add1af5652d38afe92868655cfd87e
Klodian Behrami <klodianbehrami@gmail.com>
2023-07-20 16:34:43 -0400

    phase 2 anagram

--- practice-for-ch-ruby-anagrams-long-practice/anagrams.rb
+++ practice-for-ch-ruby-anagrams-long-practice/anagrams.rb
@@ -1,27 +1,37 @@
-def permutation(str_1)
-    return [str_1] if str_1.length <= 1
+# def permutation(str_1)
+#     return [str_1] if str_1.length <= 1
 
 
-    first = str_1[0]
-    result = permutation(str_1[1..-1])
+#     first = str_1[0]
+#     result = permutation(str_1[1..-1])
 
 
-    p result
-    all_permutations = []
+#     p result
+#     all_permutations = []
 
 
-    result.each do |ele|
-      p ele
+#     result.each do |ele|
+#       p ele
 
-        (0..ele.length).each do |i|
-          p i
-          all_permutations << ele[0...i] + first + ele[i..-1]
-        end
-    end
+#         (0..ele.length).each do |i|
+#           p i
+#           all_permutations << ele[0...i] + first + ele[i..-1]
+#         end
+#     end
 
 
-    all_permutations
+#     all_permutations
 
+# end
+
+def first_anagram?(str_1, str_2)
+  return false if str_1.length != str_2.length 
+  letters = str_1.split("")
+  anagrams = letters.permutation(letters.length).to_a
+  
+  anagrams.any? {|ele| ele.join("") == str_2 }
 end
 
-p permutation("abc")
+
+
+p first_anagram?("abc", "cba")
